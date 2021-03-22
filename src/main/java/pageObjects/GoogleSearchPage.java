e88d3d3200fbcbf694b911eebadae26f40aa0067
Murali Rao <bmuralirao@gmail.com>
2021-03-22 23:40:18 +0530

    Updated the code with changes on 03/22/2021

--- src/main/java/pageObjects/GoogleSearchPage.java
+++ src/main/java/pageObjects/GoogleSearchPage.java
@@ -5,13 +5,10 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -20,6 +17,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 import utility.ConfigFileReader;
+import utility.Selenium;
 
 public class GoogleSearchPage {
 	
@@ -27,118 +25,104 @@ public class GoogleSearchPage {
 	ConfigFileReader configFileReader;
 	static ArrayList<String> searchList = new ArrayList<String>();
 	static FileWriter writer;
-	
+
 	public GoogleSearchPage(WebDriver driver) {
 		this.driver = driver;
-		 PageFactory.initElements(driver, this);
+		PageFactory.initElements(driver, this);
 	}
-	
-	 @FindBy(how = How.NAME, using = "q") 
-	 private WebElement txt_googleSearch;
-	
-	 @FindBy(how = How.NAME, using = "btnK") 
-	 private WebElement btn_googleSearch;
-	 
-	 @FindAll(@FindBy(how = How.XPATH, using="//div[@id='rso']//div[@class='hlcw0c']//div[@class='g']//div[@class='tF2Cxc']//h3"))
-	 private List<WebElement> txt_SearchList;
-	 
-	 
-	 @FindBy(how = How.TAG_NAME, using = "body") 
-	 private WebElement txt_htmlContents;
-	 
-	 	 
-	 @FindAll(@FindBy(how = How.XPATH, using = "//a"))
-	 private List<WebElement> lnk_pageLinks; 
-	 
-	 
-	 public void perform_Search(String inputKey) throws InterruptedException {
-		 txt_googleSearch.sendKeys(inputKey);
-		 JavascriptExecutor executor = (JavascriptExecutor)driver;
-		 executor.executeScript("arguments[0].click();", btn_googleSearch);
-		 Thread.sleep(5000L);
-		 }
-		 
-		 public void navigateTo_HomePage() {
-		 configFileReader = new ConfigFileReader();
-		 driver.get(configFileReader.getApplicationUrl());
-		 }
-		 
-		 public ArrayList<String> fetchSearchResults() {			 
-			 for(WebElement element:txt_SearchList) {
-				 searchList.add(element.getText());		 
-			 }
-			 return searchList;		 
-			 
-		 }
-		 
-		 public void storeSearchResultsInFile() throws IOException{
-			  configFileReader= new ConfigFileReader();	
-			  int counter=0;
-			  writer = new FileWriter(configFileReader.getFilePath(), true);
-			  for(int i=1;i<searchList.size();i++) {
-				  if(counter>10)
-					  break;
-				   counter++;
-				   writer.write(searchList.get(i));
-				   writer.write("\r\n");
-				 
-			  }	  
-			  writer.flush();
-			  //writer.close();
-			  searchList.clear();
-		 }
-		 
-		 public void displayFileContentToConsole() throws FileNotFoundException {
-			 configFileReader= new ConfigFileReader();
-			 try {
-			 File file = new File(configFileReader.getFilePath());
-			 Scanner sc = new Scanner(file);
-			 System.out.println("Displaying the file contents:");
-		      while(sc.hasNextLine()) {
-		        System.out.println(sc.nextLine());
-		      }
-		      sc.close();
-			 }catch(Exception e) {
-				 e.getStackTrace();
-			 }
-			 
-		 }
-		 
-		 public void checkLinks() throws IOException {		 
-			 System.out.println("No of links are "+ lnk_pageLinks.size()); 
-			 String singleLink="";
-			 for(int index=0;index<lnk_pageLinks.size();index++) {				
-				 WebElement links = lnk_pageLinks.get(index);
-				 singleLink = links.getAttribute("href");
-				 if(singleLink!=null) {
-				  URL url = new URL(singleLink);
-				   HttpURLConnection httpURLConnect=(HttpURLConnection)url.openConnection();
-		            httpURLConnect.setConnectTimeout(5000);
-		            httpURLConnect.connect();
-		            if(httpURLConnect.getResponseCode()>=400)
-		            {
-		             System.out.println(links.getAttribute("href")+" - "+httpURLConnect.getResponseMessage()+"is a broken link");
-		            } 
-		            else {
-		            System.out.println(links.getAttribute("href")+" - "+httpURLConnect.getResponseMessage()+" link working fine");
-		            }
-			 }
-			 }
-				 
-			 }
-		 
-		 
-		 
-		 public String getTextByBody() {
-			 return txt_htmlContents.getText();
-			 
-		 }
-			 public void close() throws IOException {
-				writer.close();
-							 }
-			 
-			 
 
-			 
-		 
+	@FindBy(how = How.NAME, using = "q")
+	private WebElement txt_googleSearch;
+	@FindBy(how = How.NAME, using = "btnK")
+	private WebElement btn_googleSearch;
+	@FindAll(@FindBy(how = How.XPATH, using = "//div[@id='rso']//div[@class='hlcw0c']//div[@class='g']//div[@class='tF2Cxc']//h3"))
+	private List<WebElement> txt_SearchList;
+	@FindBy(how = How.TAG_NAME, using = "body")
+	private WebElement txt_htmlContents;
+	@FindAll(@FindBy(how = How.XPATH, using = "//a"))
+	private List<WebElement> lnk_pageLinks;
+
+	public void perform_Search(String inputKey) throws InterruptedException {
+		Selenium sel = new Selenium();
+		txt_googleSearch.sendKeys(inputKey);
+		JavascriptExecutor executor = (JavascriptExecutor) driver;
+		executor.executeScript("arguments[0].click();", btn_googleSearch);
+		sel.waitTime();
+	}
+
+	public void openApplication() {
+		configFileReader = new ConfigFileReader();
+		driver.get(configFileReader.getApplicationUrl());
+	}
+
+	public ArrayList<String> fetchSearchResults() {
+		for (WebElement element : txt_SearchList) {
+			searchList.add(element.getText());
+		}
+		return searchList;
+
+	}
+
+	public void storeSearchResultsInFile() throws IOException {
+		configFileReader = new ConfigFileReader();
+		int counter = 0;
+		writer = new FileWriter(configFileReader.getFilePath(), true);
+		for (int i = 1; i < searchList.size(); i++) {
+			if (counter > 10)
+				break;
+			counter++;
+			writer.write(searchList.get(i));
+			writer.write("\r\n");
+
+		}
+		writer.flush();
+		searchList.clear();
+	}
+
+	public void displayFileContentToConsole() throws FileNotFoundException {
+		configFileReader = new ConfigFileReader();
+		try {
+			File file = new File(configFileReader.getFilePath());
+			Scanner sc = new Scanner(file);
+			System.out.println("Displaying the file contents:");
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+			sc.close();
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
+
+	}
+
+	public void checkLinks() throws IOException {
+		System.out.println("No of links are " + lnk_pageLinks.size());
+		String singleLink = "";
+		for (int index = 0; index < lnk_pageLinks.size(); index++) {
+			WebElement links = lnk_pageLinks.get(index);
+			singleLink = links.getAttribute("href");
+			if (singleLink != null) {
+				URL url = new URL(singleLink);
+				HttpURLConnection httpURLConnect = (HttpURLConnection) url.openConnection();
+				httpURLConnect.setConnectTimeout(5000);
+				httpURLConnect.connect();
+				if (httpURLConnect.getResponseCode() >= 400) {
+					System.out.println(links.getAttribute("href") + " - " + httpURLConnect.getResponseMessage()
+							+ "is a broken link");
+				} else {
+					System.out.println(links.getAttribute("href") + " - " + httpURLConnect.getResponseMessage()
+							+ " link working fine");
+				}
+			}
+		}
+	}
+
+	public String getTextByBody() {
+		return txt_htmlContents.getText();
+	}
+
+	public void close() throws IOException {
+		writer.close();
+	}
+
 }
--- src/test/java/stepDefinitions/GoogleSearchStepDefinition.java
+++ src/test/java/stepDefinitions/GoogleSearchStepDefinition.java
@@ -1,17 +1,7 @@
 package stepDefinitions;
 
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -19,107 +9,103 @@ import cucumber.api.java.en.When;
 import pageObjects.GoogleSearchPage;
 import utility.ConfigFileReader;
 import utility.PageObjectManager;
+import utility.Selenium;
 
 public class GoogleSearchStepDefinition {
-	 WebDriver driver;
-	 GoogleSearchPage googleSearchPage;
-	 ConfigFileReader configFileReader;
-	 PageObjectManager pageObjectManager;
-	 static int keyCounts=0;
-	 static int testCount=0;
-	 
-	 	
+	public static WebDriver driver;
+	GoogleSearchPage googleSearchPage;
+	ConfigFileReader configFileReader;
+	PageObjectManager pageObjectManager;
+	Selenium sel;
+	static int keyCounts = 0;
+	static int testCount = 0;
+
 	@Given("^user launches the google application$")
-	 public void user_launches_google_application(){
-	 configFileReader= new ConfigFileReader();
-	 System.setProperty("webdriver.chrome.driver", configFileReader.getDriverPath());
-	 driver = new ChromeDriver();
-	 driver.manage().window().maximize();
-	 driver.manage().timeouts().implicitlyWait(configFileReader.getImplicitlyWait(), TimeUnit.SECONDS);
-	 pageObjectManager = new PageObjectManager(driver);
-	 googleSearchPage = pageObjectManager.getGoogleSearchPage();
-	 googleSearchPage.navigateTo_HomePage(); 
-	 }
-	
-	@When("^user enters the search word in the application \"([^\"]*)\"$")
-	 public void user_enters_search_word_in_application(String keyWord) throws InterruptedException{
-	 configFileReader= new ConfigFileReader();
-	 pageObjectManager = new PageObjectManager(driver);
-	 googleSearchPage = pageObjectManager.getGoogleSearchPage();
-	 googleSearchPage.perform_Search(keyWord);
-	 }
-	
-	@Then("^user fetches the top 10 results$")
-	 public void user_fetches_search_result_in_application(){
-	 pageObjectManager = new PageObjectManager(driver);
-	 googleSearchPage = pageObjectManager.getGoogleSearchPage();
-	 ArrayList<String> list =googleSearchPage.fetchSearchResults();
-	 LinkedHashSet<String> set = new LinkedHashSet<String>(list);	 
-	 System.out.println("The content of list"+set);
-	 }
-	
-	@Then("^user stores the results in a file \"([^\"]*)\"$")
-	 public void user_stores_search_result_in_file(String keyWordCount) throws IOException{
-     testCount= Integer.parseInt(keyWordCount);
-     keyCounts++;    
-	 pageObjectManager = new PageObjectManager(driver);
-	 googleSearchPage = pageObjectManager.getGoogleSearchPage();
-	 googleSearchPage.storeSearchResultsInFile();
-	 }
-	
-	@Then("^user displays the values from the file to the console$")
-	 public void user_displays_result_from_file_to_console() throws IOException{		
-	if(keyCounts==testCount) {	 
-	 pageObjectManager = new PageObjectManager(driver);
-	 googleSearchPage = pageObjectManager.getGoogleSearchPage();
-	 googleSearchPage.displayFileContentToConsole();
-		} 
-	 }
-	
-	@And("^user closes the browser and other instances$")
-	 public void user_close_browser() throws IOException{		
-	 pageObjectManager = new PageObjectManager(driver);
-	 googleSearchPage = pageObjectManager.getGoogleSearchPage();	 
-	 googleSearchPage.close();
-	 driver.close();
+	public void user_launches_google_application() {
+		configFileReader = new ConfigFileReader();
+		sel = new Selenium();
+		driver = sel.driverIntialization();
+		sel.waitTime();
+		pageObjectManager = new PageObjectManager(driver);
+		googleSearchPage = pageObjectManager.getGoogleSearchPage();
+		googleSearchPage.openApplication();
 	}
-	
-	 @And("^user closes the application$")
-	 public void user_close_application() throws IOException{		
-	 driver.close();
-	 }
-	 
-	 @Then("^user checks the top 10 results links are working fine$")
-	 public void user_checks_links_in_page() throws IOException{
-	 pageObjectManager = new PageObjectManager(driver);
-	 googleSearchPage = pageObjectManager.getGoogleSearchPage();
-	 googleSearchPage.checkLinks();
-	 
-	 }
-	 
-	 
-	 
-	 @Then("^user displays the number of occurence of the keyword \"([^\"]*)\" in the page$")
-	 public void user_displays_occurence_of_keyword_in_page(String keyWord){
-	 pageObjectManager = new PageObjectManager(driver);
-	 googleSearchPage = pageObjectManager.getGoogleSearchPage();
-	 String bodyText= googleSearchPage.getTextByBody();
-	 int counter=0;	 
-	 while (bodyText.contains(keyWord)){
-		    counter++;
-		    bodyText = bodyText.substring(bodyText.indexOf(keyWord) + keyWord.length());
-		    }	     
-	    System.out.println("The number of occurence of the keyword "+keyWord+" in the page is"+counter);
-	 }
-	 
-	
-	
+
+	@When("^user enters the search word in the application \"([^\"]*)\"$")
+	public void user_enters_search_word_in_application(String keyWord) throws InterruptedException {
+		configFileReader = new ConfigFileReader();
+		pageObjectManager = new PageObjectManager(driver);
+		googleSearchPage = pageObjectManager.getGoogleSearchPage();
+		googleSearchPage.perform_Search(keyWord);
+	}
+
+	@Then("^user fetches the top 10 results$")
+	public void user_fetches_search_result_in_application() {
+		pageObjectManager = new PageObjectManager(driver);
+		googleSearchPage = pageObjectManager.getGoogleSearchPage();
+		googleSearchPage.fetchSearchResults();
+		}
+
+	@Then("^user stores the results in a file \"([^\"]*)\"$")
+	public void user_stores_search_result_in_file(String keyWordCount) throws IOException {
+		testCount = Integer.parseInt(keyWordCount);
+		keyCounts++;
+		pageObjectManager = new PageObjectManager(driver);
+		googleSearchPage = pageObjectManager.getGoogleSearchPage();
+		googleSearchPage.storeSearchResultsInFile();
+	}
+
+	@Then("^user displays the values from the file to the console$")
+	public void user_displays_result_from_file_to_console() throws IOException {
+		if (keyCounts == testCount) {
+			pageObjectManager = new PageObjectManager(driver);
+			googleSearchPage = pageObjectManager.getGoogleSearchPage();
+			googleSearchPage.displayFileContentToConsole();
+		}
+	}
+
+	@And("^user closes the browser and other instances$")
+	public void user_close_browser() throws IOException {
+		pageObjectManager = new PageObjectManager(driver);
+		googleSearchPage = pageObjectManager.getGoogleSearchPage();
+		googleSearchPage.close();
+		sel = new Selenium();
+		sel.closeBrowser();
+	}
+
+	@And("^user closes the application$")
+	public void user_close_application() throws IOException {
+		sel = new Selenium();
+		sel.closeBrowser();
+	}
+
+	@Then("^user checks the top 10 results links are working fine$")
+	public void user_checks_links_in_page() throws IOException {
+		pageObjectManager = new PageObjectManager(driver);
+		googleSearchPage = pageObjectManager.getGoogleSearchPage();
+		googleSearchPage.checkLinks();
+
+	}
+
+	@Then("^user displays the number of occurence of the keyword \"([^\"]*)\" in the page$")
+	public void user_displays_occurence_of_keyword_in_page(String keyWord) {
+		pageObjectManager = new PageObjectManager(driver);
+		googleSearchPage = pageObjectManager.getGoogleSearchPage();
+		String bodyText = googleSearchPage.getTextByBody();
+		int counter = 0;
+		while (bodyText.contains(keyWord)) {
+			counter++;
+			bodyText = bodyText.substring(bodyText.indexOf(keyWord) + keyWord.length());
+		}
+		System.out.println("The number of occurence of the keyword " + keyWord + " in the page is" + counter);
+	}
+
 	@Then("^user serach for the entered keyword \"([^\"]*)\" in the page$")
-	 public void user_search_occurence_of_keyword_in_page(String keyWord){
-	 pageObjectManager = new PageObjectManager(driver);
-	 googleSearchPage = pageObjectManager.getGoogleSearchPage();
-	 String bodyText= googleSearchPage.getTextByBody();
-	  if(bodyText.contains(keyWord)) 	     
-	    System.out.println("The entered keyword "+keyWord+" is present in the page");
-	 }
+	public void user_search_occurence_of_keyword_in_page(String keyWord) {
+		pageObjectManager = new PageObjectManager(driver);
+		googleSearchPage = pageObjectManager.getGoogleSearchPage();
+		String bodyText = googleSearchPage.getTextByBody();
+		if (bodyText.contains(keyWord))
+			System.out.println("The entered keyword " + keyWord + " is present in the page");
+	}
 }
